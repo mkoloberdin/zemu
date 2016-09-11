62b0211e3601118fb4ff72b5dac0a92d5dccf2b8
Michael Koloberdin <michael@koloberdin.com>
2016-09-12 01:40:27 +0300

    Move OS-specific stuff from defines.h to platform.h

--- src/defines.h
+++ src/defines.h
@@ -1,58 +1,14 @@
 #ifndef ZEMU_DEFINES_H
 #define ZEMU_DEFINES_H
 
-#include "platform.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
 #include <cstdint>
-
-#ifdef __unix__
-  #include <limits.h>
-#endif
+#include "platform.h"
 
 #define DEBUG_MESSAGE(msg) printf("%s\n",(msg))
-
-#ifndef MAX_PATH
-  #ifdef PATH_MAX
-    #define MAX_PATH PATH_MAX
-  #else
-    #define MAX_PATH 0x1000
-  #endif
-#endif
-
-// FIXME: SleepX() - unused. Remove?
-#ifdef _WIN32
-  #define SleepX() Sleep(1)
-#else
-  #define SleepX() sleep(0)
-#endif
-
-#ifdef __unix__
-  #define stricmp(s1,s2) strcasecmp((s1),(s2))
-  #include <unistd.h>
-#endif
-
-#ifndef M_PI
-  #define M_PI 3.1415926535897932
-#endif
-
-#ifdef __APPLE__
-  #define DRGB(r,g,b) (((b)<<0x18)|((g)<<0x10)|((r)<<8))
-  #define GETB(c) ((c)>>0x18)
-  #define GETG(c) (((c)>>0x10)&0xFF)
-  #define GETR(c) (((c)>>8)&0xFF)
-#else
-  #define DRGB(r,g,b) (((r)<<0x10)|((g)<<8)|(b))
-  #define GETR(c) ((c)>>0x10)
-  #define GETG(c) (((c)>>8)&0xFF)
-  #define GETB(c) ((c)&0xFF)
-#endif
-
-#define GET_R GETR
-#define GET_G GETG
-#define GET_B GETB
 
 extern char hex[17];
 
--- src/platform.h
+++ src/platform.h
@@ -14,6 +14,10 @@
   #define WORD_SWAP_BYTES(a) ((((a)&0xFF)<<8)|((a)>>8))
 #endif
 
+#ifdef __unix__
+  #include <limits.h>
+#endif
+
 #ifdef _WIN32
 //---------------
 // WIN32-specific
@@ -28,6 +32,46 @@
 #endif // _MSC_VER
 
 #endif // _WIN32
+
+#ifndef MAX_PATH  // defined on Windows
+  #ifdef PATH_MAX // defined on Unix
+    #define MAX_PATH PATH_MAX
+  #else
+    #define MAX_PATH 0x1000
+  #endif
+#endif
+
+// FIXME: SleepX() - unused. Remove?
+#ifdef _WIN32
+  #define SleepX() Sleep(1)
+#else
+  #define SleepX() sleep(0)
+#endif
+
+#ifdef __unix__
+  #define stricmp(s1,s2) strcasecmp((s1),(s2))
+  #include <unistd.h>
+#endif
+
+#ifndef M_PI
+  #define M_PI 3.1415926535897932
+#endif
+
+#ifdef __APPLE__
+  #define DRGB(r,g,b) (((b)<<0x18)|((g)<<0x10)|((r)<<8))
+  #define GETB(c) ((c)>>0x18)
+  #define GETG(c) (((c)>>0x10)&0xFF)
+  #define GETR(c) (((c)>>8)&0xFF)
+#else
+  #define DRGB(r,g,b) (((r)<<0x10)|((g)<<8)|(b))
+  #define GETR(c) ((c)>>0x10)
+  #define GETG(c) (((c)>>8)&0xFF)
+  #define GETB(c) ((c)&0xFF)
+#endif
+
+#define GET_R GETR
+#define GET_G GETG
+#define GET_B GETB
 
 //------
 // Other
