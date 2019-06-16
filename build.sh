c78ef61a97e1c4d8bbdc9d0de2cc3dcfd2e6b46a
restorer <restorer@zame-dev.org>
2019-06-16 11:58:20 +0300

    Fix for macOS Mojave

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+if [ "$1" = "--clean" ] ; then
+	[ -e build ] && rm -r build
+fi
+
+cmake -D CMAKE_FIND_FRAMEWORK=LAST -B build . && cmake --build build
+
+if [ "$(uname -s)" = "Darwin" ] && [ -e build/zemu ] ; then
+	ruby osx-mojave-fix.rb build/zemu
+fi
--- src/zemu.cpp
+++ src/zemu.cpp
@@ -1063,7 +1063,7 @@ void AntiFlicker(SDL_Surface *copyFrom, SDL_Surface *copyTo)
 
 		for (j = WIDTH; j--;)
 		{
-#if defined(ZEMU_BIG_ENDIAN) || defined(__APPLE__)
+#if defined(ZEMU_BIG_ENDIAN)
 			*(srw++) = 0; s1w++; s2w++;
 #endif
 
@@ -1076,7 +1076,7 @@ void AntiFlicker(SDL_Surface *copyFrom, SDL_Surface *copyTo)
 			*srw = (uint8_t)(((unsigned int)(*s1w) + (unsigned int)(*s2w)) >> 1);
 			srw++; s1w++; s2w++;
 
-#if !defined(ZEMU_BIG_ENDIAN) && !defined(__APPLE__)
+#if !defined(ZEMU_BIG_ENDIAN)
 			*(srw++) = 0; s1w++; s2w++;
 #endif
 		}
@@ -1922,7 +1922,6 @@ int main(int argc, char *argv[])
 		if (params.scale2x)
 		{
 			SDL_PixelFormat *fmt = realScreen->format;
-
 			screen = SDL_CreateRGBSurface(SDL_SWSURFACE, WIDTH, HEIGHT, fmt->BitsPerPixel, fmt->Rmask, fmt->Gmask, fmt->Bmask, 0);
 			if (screen == NULL) StrikeError("Unable to create screen surface: %s\n", SDL_GetError());
 
