f8f17a400337e4f89fc3d5d0e130600bbce5cd58
restorer <restorer@zame-dev.org>
2019-07-12 17:29:00 +0300

    All clang warnings and all issues found by PVS-Studio fixed

--- src/renderer/render_common_b.h
+++ src/renderer/render_common_b.h
@@ -1,8 +1,16 @@
-            if (clMod <= 160) {
+            //// In render_common_a.h there is:
+            //// if (clMod >= (WIDTH / SCREEN_TACTS_PER_PIXEL)) { ... continue; }
+            ////
+            //// WIDTH = 320; SCREEN_TACTS_PER_PIXEL = 2; WIDTH / SCREEN_TACTS_PER_PIXEL = 160
+            ////
+            //// So we know for sure, that clMod <= 160.
+            //// Probably this is leftover from older times, but we need to investigate more.
+            //
+            // if (clMod <= 160) {
                 clk += 4;
-            } else {
-                clk += 160 - clMod;
-            }
+            // } else {
+            //     clk += 160 - clMod;
+            // }
         }
     }
 
